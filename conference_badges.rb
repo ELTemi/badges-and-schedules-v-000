aae2a5218c5a084b8c96f25724b7055ea18898a9
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 12:23:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+require "pry"
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badge = Array.new
+  names.each do |name|
+    badge << badge_maker(name)
+  end
+  badge
+end
+
+def assign_rooms(speakers)
+  rooms_assignment = []
+  speakers.each_with_index do |speaker, room_num|
+    rooms_assignment << "Hello, #{speaker}! You'll be assigned to room #{room_num + 1}!"
+  end
+  rooms_assignment
+end
+
+def printer(names)
+  printer = []
+  #binding.pry
+  names.each do |name|
+    printer << batch_badge_creator(name)
+  end
+  printer
+end
